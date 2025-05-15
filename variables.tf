7c4acd3f5228be11f778ca861ffd4c4e5c64fda8
sureshbabu-ghb <suresh7719@gmail.com>
2025-05-15 21:00:24 +0200

    1st aws change using tf
    first terraform installation in aws

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami_id" {
+  description = "Amazon Linux 2 AMI"
+  default     = "ami-0c02fb55956c7d316" # Amazon Linux 2 in us-east-1
+}
